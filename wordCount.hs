2175015ae86a0e7c3f31d6e54b5cdc806fab39b1
Jarimatti Valkonen <jarimatti@me.com>
2021-09-30 21:05:59 +0100

    Count all in function.

--- wordCount.hs
+++ wordCount.hs
@@ -1,13 +1,11 @@
+import Distribution.Compat.Parsing (count)
 -- Display line, word, and character counts read from stdin.
 --
 -- This is a toy utility to get feet wet with Haskell.
 main :: IO ()
 main = do
   contents <- getContents
-  -- TODO: We could fold over the contents and gather everything in one go.
-  print (lineCount contents)
-  print (wordCount contents)
-  print (charCount contents)
+  print $ countAll contents
 
 lineCount :: String -> Int
 lineCount = length . lines
@@ -18,3 +16,6 @@ wordCount = length . words
 charCount :: String -> Int
 charCount = length
 
+-- Count the number of lines, words and chars in the string.
+countAll :: String -> (Int, Int, Int)
+countAll contents = (lineCount contents, wordCount contents, charCount contents)
